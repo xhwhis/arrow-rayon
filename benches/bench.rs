c480ae29ba2eb35386acf294c77a50eb7fe30b38
xhwhis <hi@whis.me>
2024-09-09 18:30:18 +0800

    chore: add benches

---
+++ benches/bench.rs
@@ -0,0 +1,40 @@
+use arrow_array::BinaryArray;
+use arrow_rayon::parallel_binary_array::BinaryArrayRefParallelIterator;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use geos::Geom;
+use rand::{thread_rng, Rng};
+use rayon::iter::{IndexedParallelIterator, ParallelIterator};
+
+fn gen_wkb_array(len: usize) -> BinaryArray {
+    let mut rng = thread_rng();
+    BinaryArray::from_iter_values((0..len).map(|_| {
+        let (x, y) = rng.gen::<(f64, f64)>();
+        geos::Geometry::new_from_wkt(&format!("POINT ({x} {y})"))
+            .unwrap()
+            .to_wkb()
+            .unwrap()
+    }))
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    for len in [1_000, 10_000, 100_000] {
+        let wkb_arr1 = gen_wkb_array(len);
+        let wkb_arr2 = gen_wkb_array(len);
+
+        c.bench_function(&format!("wkb array value distance(len = {len})"), |b| {
+            b.iter(|| {
+                wkb_arr1
+                    .par_iter()
+                    .zip(wkb_arr2.par_iter())
+                    .for_each(|(wkb1, wkb2)| {
+                        let geom1 = geos::Geometry::new_from_wkb(wkb1.unwrap()).unwrap();
+                        let geom2 = geos::Geometry::new_from_wkb(wkb2.unwrap()).unwrap();
+                        black_box(geom1.distance(&geom2).unwrap());
+                    });
+            });
+        });
+    }
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
